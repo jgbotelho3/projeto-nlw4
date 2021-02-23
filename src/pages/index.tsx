f9bfef58519b6a89a3614e68cc775c1d8204ee83
Jefferson <jgbotelho3@outlook.com>
2021-02-23 13:44:02 -0300

    Primeiro dia

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import {ExperienceBar} from '../components/ExperienceBar'
+import styles from '../styles/pages/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <ExperienceBar /> 
+    </div>
+    
+  )
+}
